e1da87e48671956051626755f0ce8aa521eb6243
kyokeefesally <kyle@wework.com>
2016-05-27 16:24:05 -0400

    extended csvmapper so that it accepts google spreadsheet key to parse remote google csv instead of local csv file

--- csv_google_parser.py
+++ csv_google_parser.py
@@ -8,24 +8,28 @@ class CSVGoogleParser(CSVParser):
 	#def __init__(self, csvFile, fmapper=None, hasHeader=False):
 	def __init__(self, spreadsheet_key, fmapper=None, hasHeader=False):
 		super(CSVGoogleParser, self).__init__(None, fmapper=fmapper, hasHeader=hasHeader)
-		# the csv file
-		#self.csvFile = csvFile
 		# the mapper object
 		self.spreadsheet_key = spreadsheet_key
-
-
 
 	# parses a CSV file
 	def parseCSV(self):
 
+		# google spreadsheet csv URL
 		CSV_URL = "https://docs.google.com/spreadsheets/d/" + self.spreadsheet_key + "/pub?output=csv"
 
+		# parse csv
 		with requests.Session() as s:
+
+			# download csv
 			download = s.get(CSV_URL)
 
+			# decode csv content
 			decoded_content = download.content.decode('utf-8')
 
+			# read csv content
 			cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+
+			# convert csv content to csv data for csvmapper
 			my_list = list(cr)
 			x = []
 			for row in my_list:
